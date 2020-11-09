fe8f33f488890ccc5ac4fb209c31ab2fa4197deb
lseen <1758250012@qq.com>
2020-11-09 11:24:56 +0800

    删除多余注释

--- eureka/src/main/java/com/springcloud/EurekaServerApplication.java
+++ eureka/src/main/java/com/springcloud/EurekaServerApplication.java
@@ -12,7 +12,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
-        //8761 这个端口是默认的，就不要修改了，后面的子项目，都会访问这个端口。
         int port = 8761;
         if (!NetUtil.isUsableLocalPort(port)) {
             System.err.printf("端口%d被占用了，无法启动%n", port);
